ce7202736d7a6e7e952abfabd1ce18eaf133b9bf
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2024-08-10 13:11:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,11 @@ with st.expander('Data'):
   st.write("**Raw Data**")
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
+
+st.write("**X-Feature**")
+X = df.drop('species',axis=1)
+X
+
+st.write("**y-Target**")
+y = df['species']
+y

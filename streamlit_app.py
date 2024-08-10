a69a7f5b3d8788df41d51e928b567b554e00eb14
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2024-08-10 14:43:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,5 +75,6 @@ clf.fit(X,y)
 prediction = clf.predict(input_row)
 prediction_prob = clf.predict_proba(input_row)
 
-prediction_prob
-
+df_pred_prob = pd.DataFrame(prediction_prob)
+df_pred_prob.columns = ['Adelie','Chinstrap','Gentoo']
+df_pred_prob

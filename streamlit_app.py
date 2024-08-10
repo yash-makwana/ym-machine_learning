a4d04df6d071b6c938096a9e66be0a0facd238d4
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2024-08-10 13:58:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,6 @@ with st.sidebar:
          }
   input_df = pd.DataFrame(data,index=[0])
   input_penguins = pd.concat([input_df,X],axis=0)
-
-input_df
+with st.expander('Input Features'):
+  st.write("**Input Selected**")
+  input_df
